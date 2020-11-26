89e74a8160146f755aad492ead9f2f01a3d97254
entchev <micishi@gmail.com>
2020-11-26 21:02:36 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const Sheet = require('./sheet');
+const fetch = require('node-fetch');
+
+async function scrapePage (i) {
+  const res = await fetch(`https://jobs.github.com/positions.json?page=${i}&search=code`);
+  const json = await res.json();
+
+  const rows = json.map(job => {
+    return {
+      company: job.company,
+      title: job.title,
+      location: job.location,
+      date: job.created_at,
+      url: job.url
+    }
+  })
+
+  return rows;
+}
+
+(async function() {
+  let i = 1; 
+  let rows =[];
+
+  while(true) {
+    const newRows = await scrapePage(i);
+    if (newRows.length === 0) break;
+    rows = rows.concat(newRows);
+    i++;
+  }
+
+  const sheet = new Sheet();
+  await sheet.load();
+  await sheet.addRows(rows);
+  })()
---
+++ sheet.js
@@ -0,0 +1,17 @@
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+module.exports = class Sheet {
+  constructor() {
+    this.doc = new GoogleSpreadsheet(''); // Google Sheet URI in the brackets
+  }
+
+  async load() {
+    await this.doc.useServiceAccountAuth(require('./credentials.json'));
+    await this.doc.loadInfo(); 
+  };
+
+  async addRows(rows) {
+    const sheet = this.doc.sheetsByIndex[0];
+    await sheet.addRows(rows); 
+  };
+}
